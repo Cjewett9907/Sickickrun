c09334294f5ef38e697135ffe9927c06c124b305
Michael <mqngo21@gmail.com>
2019-04-20 15:27:09 -0700

    added login/signup features

---
+++ front-end/src/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { firebase } from "./firebase/firebase";
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  startLogin = (e) => {
+    e.preventDefault();
+    console.log('Logging in', this.state);
+    return firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((() => {
+        console.log('success');
+      }))
+      .catch(() => {
+        console.log('failed');
+      });
+  };
+
+  startLogout = () => {
+    return () => {
+      return firebase.auth().signOut();
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="container">
+          <form className="white">
+            <h5 className="grey-text text-darken-3">Sign In</h5>
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <button className="btn blue lighten-1 z-depth-0" onClick={this.startLogin}>Login</button>
+              <button className="btn blue lighten-1 z-depth-0" onClick={() => this.props.changePage('signup')}>SignUp</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
+
+
+
